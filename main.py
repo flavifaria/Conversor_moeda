965376cf360935ba4c9b3cbd6e190e66c2b88bdc
flavifaria <lefravious@gmail.com>
2026-09-18 11:53:32 -0300

    ajuste na centralização da tela

--- main.py
+++ main.py
@@ -7,9 +7,20 @@ from pegar_conversoes import pegar_conversoes
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("green")
 
+def centralizar_janela(janela, largura=600, altura=400):
+    largura_tela = janela.winfo_screenwidth()
+    altura_tela = janela.winfo_screenheight()
+    
+    pos_x = (largura_tela // 2) - (largura // 2)
+    pos_y = (altura_tela // 2) - (altura // 2)
+    
+    janela.geometry(f"{largura}x{altura}+{pos_x}+{pos_y}")
+
+
 janela = ctk.CTk()
-janela.geometry("500x500")  # Define o tamanho da janela: largura x altura
+centralizar_janela(janela, largura=500, altura=700)
 janela.title("Conversor de Moeda")  # Define o título que aparece na janela
+
 
 
 titulo = ctk.CTkLabel(janela, text="Conversor de Moeda", font=("Arial", 20))
